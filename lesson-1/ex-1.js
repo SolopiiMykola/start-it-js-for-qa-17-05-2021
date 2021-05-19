31aa37e8fcd5ae36753948562acec48c93c58fb7
Mykola <solopiy@i.ua>
2021-05-19 22:07:09 +0300

    add first lesson code examples

---
+++ lesson-1/ex-1.js
@@ -0,0 +1,25 @@
+// let test = 1;
+// test = 2;
+// test = 3;
+// test = 4;
+// console.log(test);
+
+// const test1 = 1;
+// test1 = 2;
+
+
+
+// console.log(typeof "true");
+
+
+typeof(3); // number
+typeof("true"); // 
+typeof(true); //
+console.log(typeof(NaN)); // number
+console.log('null:', typeof(null)); // object
+console.log('undefined:', typeof(undefined)); // 
+console.log('Null', typeof(Null));
+console.log('console', typeof(console));
+console.log('console.log', typeof(console.log));
+
+// console.log(1 - '1l')
